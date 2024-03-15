285d94c594552dc9d78ae4f2535b478cae2c4448
Julian <julianszkudlarek12@gmail.com>
2024-03-15 18:52:24 +0100

    calc primitive

--- app/src/main/java/com/example/kalk/MainActivity.java
+++ app/src/main/java/com/example/kalk/MainActivity.java
@@ -13,15 +13,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Button btn1 = findViewById(R.id.button);
-        Button btn2 = findViewById(R.id.button2);
-        Button btn3 = findViewById(R.id.button3);
-        TextView text = findViewById(R.id.textView);
+        setContentView(R.layout.activity_main);
+        Button btn0 = findViewById(R.id.bt0);
+        Button btn1 = findViewById(R.id.bt1);
+        Button btn2 = findViewById(R.id.bt2);
+        Button btn3 = findViewById(R.id.bt3);
+        Button btn4 = findViewById(R.id.bt4);
+        Button btn5 = findViewById(R.id.bt5);
+        Button btn6 = findViewById(R.id.bt6);
+        Button btn7 = findViewById(R.id.bt7);
+        Button btn8 = findViewById(R.id.bt8);
+        Button btn9 = findViewById(R.id.bt9);
+        Button div = findViewById(R.id.divide);
+        Button mul = findViewById(R.id.multiply);
+        Button add = findViewById(R.id.add);
+        Button sub = findViewById(R.id.substract);
+        TextView text = findViewById(R.id.textview);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("0");
+            }
+
+        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                text.setText("Julian");
+                text.append("1");
             }
 
         });
@@ -29,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                text.setText("Hello world");
+                text.append("2");
             }
 
         });
@@ -37,7 +58,87 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                text.setText("Goodbye");
+                text.append("3");
+            }
+
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("4");
+            }
+
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("5");
+            }
+
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("6");
+            }
+
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("7");
+            }
+
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("8");
+            }
+
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("9");
+            }
+
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("/");
+            }
+
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("*");
+            }
+
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("+");
+            }
+
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.append("0");
             }
 
         });
