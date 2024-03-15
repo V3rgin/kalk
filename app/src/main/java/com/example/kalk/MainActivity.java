7571045a56f0b5f8b113d67af74f1433ee7d8edb
Julian <julianszkudlarek12@gmail.com>
2024-03-15 18:26:06 +0100

    3 buttons change what's written in the message

--- app/src/main/java/com/example/kalk/MainActivity.java
+++ app/src/main/java/com/example/kalk/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.kalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button);
+        Button btn2 = findViewById(R.id.button2);
+        Button btn3 = findViewById(R.id.button3);
+        TextView text = findViewById(R.id.textView);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.setText("Julian");
+            }
+
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.setText("Hello world");
+            }
+
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                text.setText("Goodbye");
+            }
+
+        });
     }
 }
